e0d07ca4b9ae7902a010f712d4ceaa40268a52a7
stevenvr <steven.vanrossum@student.kdg.be>
2023-03-23 16:37:23 +0100

    16:36 23/03 spel werkt, nog beetje details en styling

--- src/be/kdg/projectbasis/view/spelbord/SpelbordPresenter.java
+++ src/be/kdg/projectbasis/view/spelbord/SpelbordPresenter.java
@@ -2,9 +2,14 @@ package be.kdg.projectbasis.view.spelBord;
 
 import be.kdg.projectbasis.model.ProgrammaModel;
 import be.kdg.projectbasis.model.spelbeurten.SpelbeurtSpeler;
+import be.kdg.projectbasis.view.standaardElementen.CharacterButton;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+
+import java.awt.event.ActionListener;
 
 
 public class SpelBordPresenter {
@@ -12,15 +17,18 @@ public class SpelBordPresenter {
 
     private SpelbeurtSpeler spelbeurtSpeler;
     private SpelBordView view;
+    private int x = 0;
 
     public SpelBordPresenter(ProgrammaModel model, SpelBordView view) {
         this.model = model;
         this.view = view;
         this.spelbeurtSpeler = spelbeurtSpeler;
         addEventHandlers();
+        initializeCharacterHandlers();
     }
 
     private void addEventHandlers() {
+
         view.getBtnVraag().setOnAction(event -> {
             String question = view.getTxtVraag().getText();
             model.startSpelbeurtSpeler(question);
@@ -39,6 +47,20 @@ public class SpelBordPresenter {
             model.maakGok(gok);
         });
     }
-
+    private void initializeCharacterHandlers() {
+        CharacterButton[] characterButtons = view.getCharacterName();
+        ImageView[] characterPics = view.getCharacterPic(); // toegevoegd
+        for (int j = 0; j < characterButtons.length; j++) {
+            final int index = j; // toegevoegd
+            characterButtons[j].setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    CharacterButton button = (CharacterButton) event.getSource();
+                    button.setVisible(false); // Het karakter verdwijnt uit de view
+                    characterPics[index].setVisible(false); // toegevoegd
+                }
+            });
+        }
+    }
 
 }
